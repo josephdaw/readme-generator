c2d21dc7b08fa9d78c6ac792c831639838e3391a
josephdaw <josephdaw@icloud.com>
2021-11-03 12:13:24 +1030

    add write file function

--- index.js
+++ index.js
@@ -43,8 +43,10 @@ const questions = [
         },
 ];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// function to write README file
+function writeToFile(fileName, data) {
+    fs.writeFileSync(fileName, data)
+}
 
 // TODO: Create a function to initialize app
 function init() {}
