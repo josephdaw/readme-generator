da80c47099fffdfb8f23426d5e06cf5ece593487
josephdaw <josephdaw@icloud.com>
2021-11-03 12:10:28 +1030

    add question array

--- index.js
+++ index.js
@@ -3,8 +3,45 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 const generateMarkdown = require('./utils/generateMarkdown');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// Array of questions for user input
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the project title?',
+      },
+      {
+        type: 'input',
+        name: 'description',
+        message: 'Please provide a short description of the project.',
+      },
+      {
+        type: 'input',
+        name: 'installation',
+        message: 'Please provide instructions of how to install.',
+      },
+      {
+        type: 'input',
+        name: 'usage',
+        message: 'Please outline the usage.',
+      },
+      {
+          type: 'list',
+          name: 'license',
+          message: 'Please outline the usage.',
+          choices: ['MIT', 'GNU', 'Apache', 'Unlicense']
+        },
+        {
+          type: 'input',
+          name: 'contributing',
+          message: 'Please outline how someone can contribute to the project.',
+        },
+        {
+          type: 'input',
+          name: 'tests',
+          message: 'Please outline how to test the project.',
+        },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
